d9240aede31dcdd802e4403cfdec43975a1444db
peter varga <pkvarg@yahoo.se>
2024-06-01 16:31:19 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Knižný eshop Prúd nájdete</h1>
+        <h2>na novej adrese prud.sk</h2>
+
+        <a href='https://prud.sk' target='_blank'>
+          Ísť na novú adresu
+        </a>
+        <a href='mailto:eshop@prud.sk'>
+          <p>eshop@prud.sk</p>
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
